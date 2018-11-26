042cba0ff3c6c67106735a00fc76f183ab77a158
Evgeny Sorokin <evgerher1l@gmail.com>
2018-11-26 16:05:25 +0300

    Working solution with accuracy LogisticRegression

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,11 @@
-import breeze.linalg.DenseVector
 import org.apache.spark.ml.Pipeline
 import org.apache.spark._
 import org.apache.spark.ml.classification.{LinearSVC, LogisticRegression}
 import org.apache.spark.ml.feature.{HashingTF, LabeledPoint, Tokenizer}
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.mllib.linalg.VectorUDT
-import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.types.IntegerType
-import org.apache.spark.rdd.RDD
+
 
 object Main {
   def main(args: Array[String]) {
@@ -45,7 +42,7 @@ object Main {
       .setOutputCol("features")
 
     val lr = new LogisticRegression()
-      .setMaxIter(10)
+      .setMaxIter(100)
       .setRegParam(0.001)
 
     val pipeline = new Pipeline()
@@ -60,7 +57,18 @@ object Main {
     println(model)
     println("###" * 10)
     val observations = model.transform(test.toDF("ItemID", "Sentiment", "SentimentText"))
-      .select("probability")
+
+    val predictionLabelsRDD = observations.select("prediction", "Sentiment").rdd.map { r =>
+      val a = java.lang.Double.parseDouble(r.get(0).toString)
+      val b = java.lang.Double.parseDouble(r.get(1).toString)
+      (a * 1.0, b * 1.0)
+    }
+    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+    val precision = metrics.precisionByThreshold
+    precision.foreach { case (t, p) =>
+      println(s"Threshold: $t, Precision: $p")
+    }
+
     session.stop()
   }
 }
