dc16ef4e79cf47b7f6adce543860b57cc73b3502
the_art_of_war <xildar98@mail.ru>
2018-11-28 20:06:37 +0300

    cv and finding best parametrs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,14 +6,20 @@ import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.types.IntegerType
 
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 
+import org.apache.log4j.{Level, Logger}
 object Main {
   def main(args: Array[String]) {
     val config = new SparkConf()
       .setMaster("local[*]")
       .setAppName("Test app")
       .set("spark.driver.bindAddress", "127.0.0.1") // todo: remove it later
-//    val sc = new SparkContext(config)
+    //    val sc = new SparkContext(config)
+
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
 
     val session = SparkSession.builder()
       .config(config)
@@ -24,11 +30,16 @@ object Main {
     val training = session.read
       .format("csv")
       .option("header", "true")
-      .load("file:///C:/cygwin64/home/evger/twitter-classifier/src/main/resources/train.csv")
+      .load("file:///C:/Users/the_art_of_war/IdeaProjects/twitter-classifier/src/main/resources//train.csv")
     val df = training.withColumn("Sentiment", training.col("Sentiment").cast(IntegerType))
-    df.show(20)
+    //df.show(20)
+
 
     val Array(train, test) = df.randomSplit(Array[Double](0.7, 0.3))
+
+    val all = df.toDF("ItemID","label","SentimentText")
+
+    all.show(20)
 
     val dfs = train.toDF("ItemID", "label", "SentimentText")
 
@@ -48,27 +59,69 @@ object Main {
     val pipeline = new Pipeline()
       .setStages(Array(tokenizer, hashingTF, lr))
 
-//    val lsvc = new LinearSVC()
-//      .setMaxIter(10)
-//      .setRegParam(0.1)
+    //    val lsvc = new LinearSVC()
+    //      .setMaxIter(10)
+    //      .setRegParam(0.1)
 
-    val model = pipeline.fit(dfs)
-    println("###" * 10)
-    println(model)
-    println("###" * 10)
-    val observations = model.transform(test.toDF("ItemID", "Sentiment", "SentimentText"))
+//    val model = pipeline.fit(dfs)
+//    println("###" * 10)
+//    println(model)
+//    println("###" * 10)
+//    val observations = model.transform(test.toDF("ItemID", "Sentiment", "SentimentText"))
+//
+//    val predictionLabelsRDD = observations.select("prediction", "Sentiment").rdd.map { r =>
+//      val a = java.lang.Double.parseDouble(r.get(0).toString)
+//      val b = java.lang.Double.parseDouble(r.get(1).toString)
+//      (a * 1.0, b * 1.0)
+//    }
+//    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+//
+//    val precision = metrics.precisionByThreshold
 
-    val predictionLabelsRDD = observations.select("prediction", "Sentiment").rdd.map { r =>
-      val a = java.lang.Double.parseDouble(r.get(0).toString)
-      val b = java.lang.Double.parseDouble(r.get(1).toString)
-      (a * 1.0, b * 1.0)
-    }
-    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
-    val precision = metrics.precisionByThreshold
-    precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
-    }
+//    println("\n\n\n\n\n\n\n\n\n\n\n")
+//    precision.foreach { case (t, p) =>
+//      println(s"Threshold: $t, Precision: $p")
+//    }
+//    println("\n\n\n\n\n\n\n\n\n\n\n")
+
+   // val temp = config.longAccumulator("counter")
+//
+//    val counter = SparkContext.accumulator(0)
+//    predictionLabelsRDD.foreach {
+//      x => counter.add( (if (Math.abs(x._1 - x._2 ) < 0.5 ) 1 else 0) )
+//    }
+//
+//    val accuracy_value = counter.value / predictionLabelsRDD.count();
+//
+//    println("\n\n\n\n\n\n\n\n\n\n\n")
+//    println("Counter " + counter.value)
+//    println("Accuracy " + accuracy_value)
+//    println("\n\n\n\n\n\n\n\n")
+
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
+      .addGrid(lr.regParam, Array(0.1, 0.01))
+      .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
+
+    val cv = new CrossValidator()
+      .setEstimator(pipeline) // provide your pipeline
+      .setEvaluator(new BinaryClassificationEvaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(3) // Use 3+ in practice
+      .setParallelism(2) // Evaluate up to 2 parameter settings in parallel
+
+    val cvModel = cv.fit(all);
+
+    println("*\n*\n*\n*\n*\n*\n*\n")
+    val x = cvModel.getEstimatorParamMaps
+      .zip(cvModel.avgMetrics)
+      .maxBy(_._2)
+      ._1;
+    print(x)
+    println("*\n*\n*\n*\n*\n*\n*\n")
 
     session.stop()
   }
 }
+
+
