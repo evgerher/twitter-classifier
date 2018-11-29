34c07527baa4ce9c6e4d65e9da645a5ad081d296
the_art_of_war <xildar98@mail.ru>
2018-11-29 18:28:36 +0300

    saving model and answering query

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark._
 import org.apache.spark.ml.classification.{LinearSVC, LogisticRegression}
 import org.apache.spark.ml.feature.{HashingTF, LabeledPoint, Tokenizer}
@@ -7,10 +7,20 @@ import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.types.IntegerType
 
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+
+
+
 
 import org.apache.log4j.{Level, Logger}
 object Main {
+
   def main(args: Array[String]) {
     val config = new SparkConf()
       .setMaster("local[*]")
@@ -32,6 +42,7 @@ object Main {
       .option("header", "true")
       .load("file:///C:/Users/the_art_of_war/IdeaProjects/twitter-classifier/src/main/resources//train.csv")
     val df = training.withColumn("Sentiment", training.col("Sentiment").cast(IntegerType))
+  //  val df = training.withColumn("Sentiment", training.col("Sentiment").cast(DoubleType))
     //df.show(20)
 
 
@@ -41,8 +52,57 @@ object Main {
 
     all.show(20)
 
-    val dfs = train.toDF("ItemID", "label", "SentimentText")
+    val train_df = train.toDF("ItemID", "label", "SentimentText")
+    val test_df = test.toDF("ItemID", "label", "SentimentText")
 
+    val tokenizer = new Tokenizer()
+      .setInputCol("SentimentText")
+      .setOutputCol("Variants")
+
+    val hashingTF = new HashingTF()
+      .setNumFeatures(5000)
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("features")
+
+    val lr = new LogisticRegression()
+      .setMaxIter(100)
+      .setRegParam(0.1)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, hashingTF, lr))
+
+   // findBestParamsOfLogistic(train_df,test_df)
+
+   // findBestParamsOfGradientBoosting(train_df,test_df)
+
+    class Query {
+
+
+      def get(test : DataFrame) : DataFrame =  {
+        val path = "Model"
+        val local_path = "file:///C:/Users/the_art_of_war/IdeaProjects/twitter-classifier/model"
+        val model = PipelineModel.load(local_path)
+        val observations = model.transform(test)
+        println("OK!")
+        return observations
+      }
+    }
+
+    val model = pipeline.fit(all);
+    val path = "Model"
+    val local_path = "file:///C:/Users/the_art_of_war/IdeaProjects/twitter-classifier/model"
+    model.write.overwrite().save(local_path);
+    println("OK! saved model")
+
+
+    val caller = new Query()
+    val res = caller.get(test_df)
+    res.show(20);
+
+    session.stop()
+  }
+
+  def findBestParamsOfLogistic(train: DataFrame, test : DataFrame) : Unit = {
     val tokenizer = new Tokenizer()
       .setInputCol("SentimentText")
       .setOutputCol("Variants")
@@ -59,68 +119,119 @@ object Main {
     val pipeline = new Pipeline()
       .setStages(Array(tokenizer, hashingTF, lr))
 
-    //    val lsvc = new LinearSVC()
-    //      .setMaxIter(10)
-    //      .setRegParam(0.1)
-
-//    val model = pipeline.fit(dfs)
-//    println("###" * 10)
-//    println(model)
-//    println("###" * 10)
-//    val observations = model.transform(test.toDF("ItemID", "Sentiment", "SentimentText"))
-//
-//    val predictionLabelsRDD = observations.select("prediction", "Sentiment").rdd.map { r =>
-//      val a = java.lang.Double.parseDouble(r.get(0).toString)
-//      val b = java.lang.Double.parseDouble(r.get(1).toString)
-//      (a * 1.0, b * 1.0)
-//    }
-//    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
-//
-//    val precision = metrics.precisionByThreshold
-
-//    println("\n\n\n\n\n\n\n\n\n\n\n")
-//    precision.foreach { case (t, p) =>
-//      println(s"Threshold: $t, Precision: $p")
-//    }
-//    println("\n\n\n\n\n\n\n\n\n\n\n")
-
-   // val temp = config.longAccumulator("counter")
-//
-//    val counter = SparkContext.accumulator(0)
-//    predictionLabelsRDD.foreach {
-//      x => counter.add( (if (Math.abs(x._1 - x._2 ) < 0.5 ) 1 else 0) )
-//    }
-//
-//    val accuracy_value = counter.value / predictionLabelsRDD.count();
-//
-//    println("\n\n\n\n\n\n\n\n\n\n\n")
-//    println("Counter " + counter.value)
-//    println("Accuracy " + accuracy_value)
-//    println("\n\n\n\n\n\n\n\n")
-
     val paramGrid = new ParamGridBuilder()
-      .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
-      .addGrid(lr.regParam, Array(0.1, 0.01))
+      .addGrid(hashingTF.numFeatures, Array(1000,2000,5000,10000))
+      .addGrid(lr.regParam, Array(0.1, 0.01,1.0))
       .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
 
     val cv = new CrossValidator()
       .setEstimator(pipeline) // provide your pipeline
-      .setEvaluator(new BinaryClassificationEvaluator)
+      .setEvaluator(new BinaryClassificationEvaluator())
       .setEstimatorParamMaps(paramGrid)
       .setNumFolds(3) // Use 3+ in practice
       .setParallelism(2) // Evaluate up to 2 parameter settings in parallel
 
-    val cvModel = cv.fit(all);
+    val cvModel = cv.fit(train);
 
-    println("*\n*\n*\n*\n*\n*\n*\n")
-    val x = cvModel.getEstimatorParamMaps
+    println("Best params for model")
+    val bestParams = cvModel.getEstimatorParamMaps
       .zip(cvModel.avgMetrics)
       .maxBy(_._2)
       ._1;
-    print(x)
-    println("*\n*\n*\n*\n*\n*\n*\n")
+    print(bestParams)
+    println("")
 
-    session.stop()
+    val observations = cvModel.transform(test)
+
+    val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
+      val a = java.lang.Double.parseDouble(r.get(0).toString)
+      val b = java.lang.Double.parseDouble(r.get(1).toString)
+      (a * 1.0, b * 1.0)
+    }
+    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+
+    val precision = metrics.precisionByThreshold
+
+    val recall = metrics.recallByThreshold
+
+    val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
+
+    println("\n\n\n\n\n")
+    precision.foreach { case (t, p) =>
+      println(s"Threshold: $t, Precision: $p")
+    }
+    recall.foreach { case (t, p) =>
+      println(s"Threshold: $t, Recall : $p")
+    }
+    println("Accuracy : "+ metrics1.accuracy)
+
+    println("\n\n\n\n\n")
+  }
+
+
+  def findBestParamsOfGradientBoosting(train: DataFrame, test : DataFrame) : Unit = {
+    val tokenizer = new Tokenizer()
+      .setInputCol("SentimentText")
+      .setOutputCol("Variants")
+
+    val hashingTF = new HashingTF()
+      .setNumFeatures(1000)
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("features")
+
+    val gbt = new GBTClassifier()
+      .setMaxIter(10)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, hashingTF, gbt))
+
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(hashingTF.numFeatures, Array(100,1000))
+      .addGrid(gbt.maxIter, Array(1,2,5))
+      .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
+
+    val cv = new CrossValidator()
+      .setEstimator(pipeline) // provide your pipeline
+      .setEvaluator(new BinaryClassificationEvaluator())
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(3) // Use 3+ in practice
+      .setParallelism(4) // Evaluate up to 2 parameter settings in parallel
+
+    val cvModel = cv.fit(train);
+
+    println("Best params for model")
+    val bestParams = cvModel.getEstimatorParamMaps
+      .zip(cvModel.avgMetrics)
+      .maxBy(_._2)
+      ._1;
+    print(bestParams)
+    println("")
+
+    val observations = cvModel.transform(test)
+
+    val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
+      val a = java.lang.Double.parseDouble(r.get(0).toString)
+      val b = java.lang.Double.parseDouble(r.get(1).toString)
+      (a * 1.0, b * 1.0)
+    }
+    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+
+    val precision = metrics.precisionByThreshold
+
+    val recall = metrics.recallByThreshold
+
+    val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
+
+    println("\n\n\n\n\n")
+    precision.foreach { case (t, p) =>
+      println(s"Threshold: $t, Precision: $p")
+    }
+    recall.foreach { case (t, p) =>
+      println(s"Threshold: $t, Recall : $p")
+    }
+    println("Accuracy : "+ metrics1.accuracy)
+
+    println("\n\n\n\n\n")
   }
 }
 
