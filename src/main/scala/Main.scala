ca2fdd7d2cdc21f2f2afb2a1e9fd4d4a0404c6c9
the_art_of_war <xildar98@mail.ru>
2018-11-30 21:48:00 +0300

    ML model for CV

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -46,7 +46,7 @@ object Main {
     //df.show(20)
 
 
-    val Array(train, test) = df.randomSplit(Array[Double](0.7, 0.3))
+    val Array(train, test) = df.randomSplit(Array[Double](0.7, 0.3), seed = 28)
 
     val all = df.toDF("ItemID","label","SentimentText")
 
@@ -54,16 +54,219 @@ object Main {
 
     val temp = new Model()
 
-
-
     val train_df = train.toDF("ItemID", "label", "SentimentText")
     val test_df = test.toDF("ItemID", "label", "SentimentText")
 
-   // temp.train(train_df)
+    val findModel = new MLFindModel()
+    findModel.findBestParamsOfSVM(train_df,test_df)
+    return
+
+    temp.train(train_df)
     val res = temp.get(test_df)
     res.show(20)
 
     session.stop()
+  }
+
+  class MLFindModel{
+    def findBestParamsOfLogistic(train: DataFrame, test : DataFrame) : Unit = {
+      val tokenizer = new Tokenizer()
+        .setInputCol("SentimentText")
+        .setOutputCol("Variants")
+
+      val hashingTF = new HashingTF()
+        .setNumFeatures(1000)
+        .setInputCol(tokenizer.getOutputCol)
+        .setOutputCol("features")
+
+      val lr = new LogisticRegression()
+        .setMaxIter(100)
+        .setRegParam(0.001)
+
+      val pipeline = new Pipeline()
+        .setStages(Array(tokenizer, hashingTF, lr))
+
+      val paramGrid = new ParamGridBuilder()
+        .addGrid(hashingTF.numFeatures, Array(1000,2000,5000,10000))
+        .addGrid(lr.regParam, Array(0.1, 0.01,1.0))
+        .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
+
+      val cv = new CrossValidator()
+        .setEstimator(pipeline) // provide your pipeline
+        .setEvaluator(new BinaryClassificationEvaluator())
+        .setEstimatorParamMaps(paramGrid)
+        .setNumFolds(3) // Use 3+ in practice
+        .setParallelism(4) // Evaluate up to 2 parameter settings in parallel
+
+      val cvModel = cv.fit(train);
+
+      println("Best params for model")
+      val bestParams = cvModel.getEstimatorParamMaps
+        .zip(cvModel.avgMetrics)
+        .maxBy(_._2)
+        ._1;
+      print(bestParams)
+      println("")
+
+      val observations = cvModel.transform(test)
+
+      val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
+        val a = java.lang.Double.parseDouble(r.get(0).toString)
+        val b = java.lang.Double.parseDouble(r.get(1).toString)
+        (a * 1.0, b * 1.0)
+      }
+      val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+
+      val precision = metrics.precisionByThreshold
+
+      val recall = metrics.recallByThreshold
+
+      val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
+
+      println("\n\n\n\n\n")
+      precision.foreach { case (t, p) =>
+        println(s"Threshold: $t, Precision: $p")
+      }
+      recall.foreach { case (t, p) =>
+        println(s"Threshold: $t, Recall : $p")
+      }
+      println("Accuracy : "+ metrics1.accuracy)
+
+      println("\n\n\n\n\n")
+    }
+
+    def findBestParamsOfGradientBoosting(train: DataFrame, test : DataFrame) : Unit = {
+      val tokenizer = new Tokenizer()
+        .setInputCol("SentimentText")
+        .setOutputCol("Variants")
+
+      val hashingTF = new HashingTF()
+        .setNumFeatures(1000)
+        .setInputCol(tokenizer.getOutputCol)
+        .setOutputCol("features")
+
+      val gbt = new GBTClassifier()
+        .setMaxIter(10)
+
+      val pipeline = new Pipeline()
+        .setStages(Array(tokenizer, hashingTF, gbt))
+
+      val paramGrid = new ParamGridBuilder()
+        .addGrid(hashingTF.numFeatures, Array(100,1000))
+        .addGrid(gbt.maxIter, Array(1,2,5))
+        .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
+
+      val cv = new CrossValidator()
+        .setEstimator(pipeline) // provide your pipeline
+        .setEvaluator(new BinaryClassificationEvaluator())
+        .setEstimatorParamMaps(paramGrid)
+        .setNumFolds(3) // Use 3+ in practice
+        .setParallelism(4) // Evaluate up to 2 parameter settings in parallel
+
+      val cvModel = cv.fit(train);
+
+      println("Best params for model")
+      val bestParams = cvModel.getEstimatorParamMaps
+        .zip(cvModel.avgMetrics)
+        .maxBy(_._2)
+        ._1;
+      print(bestParams)
+      println("")
+
+      val observations = cvModel.transform(test)
+
+      val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
+        val a = java.lang.Double.parseDouble(r.get(0).toString)
+        val b = java.lang.Double.parseDouble(r.get(1).toString)
+        (a * 1.0, b * 1.0)
+      }
+      val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+
+      val precision = metrics.precisionByThreshold
+
+      val recall = metrics.recallByThreshold
+
+      val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
+
+      println("\n\n\n\n\n")
+      precision.foreach { case (t, p) =>
+        println(s"Threshold: $t, Precision: $p")
+      }
+      recall.foreach { case (t, p) =>
+        println(s"Threshold: $t, Recall : $p")
+      }
+      println("Accuracy : "+ metrics1.accuracy)
+
+      println("\n\n\n\n\n")
+    }
+
+    def findBestParamsOfSVM(train: DataFrame, test : DataFrame) : Unit = {
+      val tokenizer = new Tokenizer()
+        .setInputCol("SentimentText")
+        .setOutputCol("Variants")
+
+      val hashingTF = new HashingTF()
+        .setNumFeatures(1000)
+        .setInputCol(tokenizer.getOutputCol)
+        .setOutputCol("features")
+
+      val lsvc = new LinearSVC()
+        .setMaxIter(10)
+        .setRegParam(0.1)
+
+      val pipeline = new Pipeline()
+        .setStages(Array(tokenizer, hashingTF, lsvc))
+
+      val paramGrid = new ParamGridBuilder()
+        .addGrid(hashingTF.numFeatures, Array(1000,5000))
+        .addGrid(lsvc.regParam, Array(0.1, 0.01,1.0))
+        .addGrid(lsvc.maxIter, Array(5, 10))
+        .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
+
+      val cv = new CrossValidator()
+        .setEstimator(pipeline) // provide your pipeline
+        .setEvaluator(new BinaryClassificationEvaluator())
+        .setEstimatorParamMaps(paramGrid)
+        .setNumFolds(3) // Use 3+ in practice
+        .setParallelism(4) // Evaluate up to 2 parameter settings in parallel
+
+      val cvModel = cv.fit(train);
+
+      println("Best params for model")
+      val bestParams = cvModel.getEstimatorParamMaps
+        .zip(cvModel.avgMetrics)
+        .maxBy(_._2)
+        ._1;
+      print(bestParams)
+      println("")
+
+      val observations = cvModel.transform(test)
+
+      val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
+        val a = java.lang.Double.parseDouble(r.get(0).toString)
+        val b = java.lang.Double.parseDouble(r.get(1).toString)
+        (a * 1.0, b * 1.0)
+      }
+      val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
+
+      val precision = metrics.precisionByThreshold
+
+      val recall = metrics.recallByThreshold
+
+      val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
+
+      println("\n\n\n\n\n")
+      precision.foreach { case (t, p) =>
+        println(s"Threshold: $t, Precision: $p")
+      }
+      recall.foreach { case (t, p) =>
+        println(s"Threshold: $t, Recall : $p")
+      }
+      println("Accuracy : "+ metrics1.accuracy)
+
+      println("\n\n\n\n\n")
+    }
+
   }
 
   class Model{
@@ -103,137 +306,7 @@ object Main {
 
   }
 
-  def findBestParamsOfLogistic(train: DataFrame, test : DataFrame) : Unit = {
-    val tokenizer = new Tokenizer()
-      .setInputCol("SentimentText")
-      .setOutputCol("Variants")
 
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(tokenizer.getOutputCol)
-      .setOutputCol("features")
-
-    val lr = new LogisticRegression()
-      .setMaxIter(100)
-      .setRegParam(0.001)
-
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, hashingTF, lr))
-
-    val paramGrid = new ParamGridBuilder()
-      .addGrid(hashingTF.numFeatures, Array(1000,2000,5000,10000))
-      .addGrid(lr.regParam, Array(0.1, 0.01,1.0))
-      .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
-
-    val cv = new CrossValidator()
-      .setEstimator(pipeline) // provide your pipeline
-      .setEvaluator(new BinaryClassificationEvaluator())
-      .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(3) // Use 3+ in practice
-      .setParallelism(2) // Evaluate up to 2 parameter settings in parallel
-
-    val cvModel = cv.fit(train);
-
-    println("Best params for model")
-    val bestParams = cvModel.getEstimatorParamMaps
-      .zip(cvModel.avgMetrics)
-      .maxBy(_._2)
-      ._1;
-    print(bestParams)
-    println("")
-
-    val observations = cvModel.transform(test)
-
-    val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
-      val a = java.lang.Double.parseDouble(r.get(0).toString)
-      val b = java.lang.Double.parseDouble(r.get(1).toString)
-      (a * 1.0, b * 1.0)
-    }
-    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
-
-    val precision = metrics.precisionByThreshold
-
-    val recall = metrics.recallByThreshold
-
-    val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
-
-    println("\n\n\n\n\n")
-    precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
-    }
-    recall.foreach { case (t, p) =>
-      println(s"Threshold: $t, Recall : $p")
-    }
-    println("Accuracy : "+ metrics1.accuracy)
-
-    println("\n\n\n\n\n")
-  }
-
-
-  def findBestParamsOfGradientBoosting(train: DataFrame, test : DataFrame) : Unit = {
-    val tokenizer = new Tokenizer()
-      .setInputCol("SentimentText")
-      .setOutputCol("Variants")
-
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(tokenizer.getOutputCol)
-      .setOutputCol("features")
-
-    val gbt = new GBTClassifier()
-      .setMaxIter(10)
-
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, hashingTF, gbt))
-
-    val paramGrid = new ParamGridBuilder()
-      .addGrid(hashingTF.numFeatures, Array(100,1000))
-      .addGrid(gbt.maxIter, Array(1,2,5))
-      .build() // Get 3 by 2 grid with 6 parameter pairs to evaluate
-
-    val cv = new CrossValidator()
-      .setEstimator(pipeline) // provide your pipeline
-      .setEvaluator(new BinaryClassificationEvaluator())
-      .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(3) // Use 3+ in practice
-      .setParallelism(4) // Evaluate up to 2 parameter settings in parallel
-
-    val cvModel = cv.fit(train);
-
-    println("Best params for model")
-    val bestParams = cvModel.getEstimatorParamMaps
-      .zip(cvModel.avgMetrics)
-      .maxBy(_._2)
-      ._1;
-    print(bestParams)
-    println("")
-
-    val observations = cvModel.transform(test)
-
-    val predictionLabelsRDD = observations.select("prediction", "label").rdd.map { r =>
-      val a = java.lang.Double.parseDouble(r.get(0).toString)
-      val b = java.lang.Double.parseDouble(r.get(1).toString)
-      (a * 1.0, b * 1.0)
-    }
-    val metrics = new BinaryClassificationMetrics(predictionLabelsRDD)
-
-    val precision = metrics.precisionByThreshold
-
-    val recall = metrics.recallByThreshold
-
-    val metrics1 = new MulticlassMetrics(predictionLabelsRDD)
-
-    println("\n\n\n\n\n")
-    precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
-    }
-    recall.foreach { case (t, p) =>
-      println(s"Threshold: $t, Recall : $p")
-    }
-    println("Accuracy : "+ metrics1.accuracy)
-
-    println("\n\n\n\n\n")
-  }
 }
 
 
